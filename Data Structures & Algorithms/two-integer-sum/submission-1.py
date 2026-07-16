26467a2a15ee40a9d72998ea0e6d9ea68fcf48a9
Esha Pandey <46835432+eshapandey0530@users.noreply.github.com>
2026-07-16 14:12:56 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # brute force - nested loop - all pairs are compared -> ~O(n^2)
+        # space -> O(1)
+        # works for sorted and unsorted array
+
+        # uses hashmap for maintaing and saving indexes
+        # 2 ways -> either create a hashmap in the beginning and put a condition to check the indexes are not same
+            # or -> add the index and element in the hashmap one by one as we check the difference is existing in the array or not
+
+        # in this we have our auxilary space to be ~O(n) given the diff is not found in the array and all the array elements are added in the hashmap
+
+        hashmap = {}
+
+        for i in range(len(nums)):
+            
+            diff = target - nums[i]
+
+            if diff in hashmap:
+                return [hashmap[diff], i]
+
+            else:
+                hashmap[nums[i]] = i
+
+        return None
+        
