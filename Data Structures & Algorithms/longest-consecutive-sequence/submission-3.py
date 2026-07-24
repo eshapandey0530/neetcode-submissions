5d425c2b690b0d5530330d9e2cd32d2e32c6926a
Esha Pandey <46835432+eshapandey0530@users.noreply.github.com>
2026-07-24 15:31:06 -0400

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,55 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        # iterate through the array
+        # check previous number exist or not in the array from the current selection
+        # when found a starting point, start checking for the consecutive sequence
+        # use a count variable to keep a track 
+        # use hash set to check visited numbers
+
+        hset = set(nums)
+        res = 0
+
+        for num in nums:
+
+            if num - 1 not in nums:
+                count = 1
+                next_num = num + 1
+                while next_num in hset:
+                    count += 1
+                    next_num += 1
+                res = max(res, count)
+
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # hset = set(nums)
+        # res = 0
+
+        # for n in nums:
+        #     if n - 1 not in hset:
+        #         count = 1
+        #         next_n = n + 1
+        #         while next_n in hset:
+        #             count += 1
+        #             next_n += 1
+        #         res = max(res, count)
+
+        # return res
